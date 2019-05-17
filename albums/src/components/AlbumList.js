42cbfbe67d8aeeba58b5f6335cca7f8c503b9003
Jeremy Sykes <jsykes@PHY-101640.local>
2019-05-17 15:22:18 -0400

    As a developer I have started adding the albums functionality (Work in progress)

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,35 @@
+
+// Example of class based component
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+    componentWillMount() {
+        axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => {
+                this.setState({ albums: response.data });
+            });
+    }
+
+    renderAlbums() {
+        // Typically the key would be passed in as an id with the data, but this will work
+        return this.state.albums.map(album => 
+        <AlbumDetail key={album.title} album={album} />);
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View >
+        );
+    }
+}
+
+export default AlbumList;
--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,3 +1,6 @@
+
+// Example of functional component
+
 import React from 'react';
 import { Text, View } from 'react-native';
 
