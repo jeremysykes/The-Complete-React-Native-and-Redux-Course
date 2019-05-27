b99a6d9a1acb55190651856adf66772c7b070127
Jeremy Sykes <jsykes@PHY-101640.local>
2019-05-27 16:23:46 -0400

    As a developer I finished section 8: handling component layout.

--- albums/App.js
+++ albums/App.js
@@ -3,8 +3,10 @@ import { View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
+// Need the syle of flex: 1 so that the given component 
+// will expand to the entire height of the device.
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header text={'Albums'} />
     <AlbumList />
   </View>
