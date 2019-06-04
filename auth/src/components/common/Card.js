14153c21b06b18c29a6def2c1b4dc3d306d0c008
Jeremy Sykes <jsykes@PHY-101640.local>
2019-06-04 10:53:56 -0400

    As a developer I have reached section 9 video 57

---
+++ auth/src/components/common/Card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => (
+    <View style={styles.containerStyle}>
+        {props.children}
+    </View>
+);
+
+// To set 'borderBottomWidth' you either need all other sides explicitly 
+// set OR set the 'backgroundColor' before hand
+const styles = {
+    containerStyle: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
+};
+
+export { Card };
