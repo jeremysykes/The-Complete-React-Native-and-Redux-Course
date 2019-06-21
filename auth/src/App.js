ee8e7d04f5e868c2d3f87ae7d55befd53fa13eca
jeremysykes <50718494+jeremysykes@users.noreply.github.com>
2019-06-21 10:33:33 -0400

    Update App.js

--- auth/src/App.js
+++ auth/src/App.js
@@ -11,13 +11,7 @@ class App extends Component {
   };
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyCd98qbv7KxXjiFM2Z0WcB-sfr3BRxcPmQ',
-      authDomain: 'auth-35bf0.firebaseapp.com',
-      databaseURL: 'https://auth-35bf0.firebaseio.com',
-      projectId: 'auth-35bf0',
-      storageBucket: 'auth-35bf0.appspot.com',
-      messagingSenderId: '444891106942',
-      appId: '1:444891106942:web:0997d440f8f3770f'
+      // Put initialization parameters from firebase here
     });
 
     // User for handling when a user signs in or out
