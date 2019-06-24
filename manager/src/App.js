f8c1225a20d8bcaa81a63a06f14d21dd64800f0b
Jeremy Sykes <jsykes@phy-412a.tvo.org>
2019-06-24 17:26:26 -0400

    As a developer I finished section 16 of the course - still need to troubleshoot entering text into form fields.

---
+++ manager/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDbx1RVQoPjaAavVZUNYM3uSfJEXn_U-vc',
+      authDomain: 'manager-561cc.firebaseapp.com',
+      databaseURL: 'https://manager-561cc.firebaseio.com',
+      projectId: 'manager-561cc',
+      storageBucket: 'manager-561cc.appspot.com',
+      messagingSenderId: '23938797748',
+      appId: '1:23938797748:web:58227b0141ef89ab'
+    };
+
+    firebase.initializeApp(config);
+  }
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={ store }>
+        <LoginForm></LoginForm>
+      </Provider>
+    );
+  }
+}
+
+export default App;
